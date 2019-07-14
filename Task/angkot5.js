cad36ab596dc3e06ecc9ae7555e7af7c3fcf47b7
Arryexe <arianur098@gmail.com>
2019-07-14 16:33:57 +0800

    Task - Angkot : Loop (For + Conditional Statement + Logical Operator)

---
+++ Task/angkot5.js
@@ -0,0 +1,14 @@
+var jumlahAngkot = 10;
+var angkotBeroprasi = 5;
+var angkot = 1;
+
+for ( ;angkot <= jumlahAngkot; angkot++) {
+	if (angkot <= 6) {
+		console.log('Angkot No. ' + angkot + ' beroperasi dengan baik.');
+	} else if (angkot === 8 || angkot === 10) {
+		console.log('Angkot No. ' + angkot + ' sedang lembur');
+	}
+	 else {
+		console.log('Angkot No. ' + angkot + ' sedang tidak beroperasi.');
+	}
+}
